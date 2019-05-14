f0011d95985b148c3dd9071754d5d0b30e32f6cb
jingyanjun <45578739+jingyanjun@users.noreply.github.com>
2019-05-14 18:35:29 -0500

    Create update.js

---
+++ WebContent/js/update.js
@@ -0,0 +1,30 @@
+/*created by Yanjun*/
+
+$(function(){
+	$("#updateBtnId").on('click',function(event){
+		event.preventDefault();			
+		var title = $("#titleId").val();
+		var shortDescription = $("#shortDescriptionId").val();
+		var msg = "";
+
+        if (title.trim().length == 0) {
+            msg += "Title field is required! ";
+        }
+        if (shortDescription.trim().length == 0) {
+            msg += "Short Description is required! ";
+        }
+       
+        if (msg.trim().length != 0) {
+        	alert(msg);
+            return;
+        }
+		
+		$("#updatePostingFormId").submit();
+	});
+		
+	$("#cancelBtnId").on('click',function(event){
+		event.preventDefault();
+		window.location = 'main';
+	});	
+	
+});
